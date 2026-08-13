3894ab22b05b0099b9343621aa0c5eaa5dc668bd
Kevin Bauer <kevin.bauer@byu.edu>
2026-08-12 20:49:58 -0600

    first commit

---
+++ js/dragon.js
@@ -0,0 +1,28 @@
+const eggCounts = {
+    water: 0,
+    fire: 0,
+    electric: 0,
+};
+const MAX_EGG_COUNTS = {
+    water: 1,
+    fire: 1,
+    electric: 1,
+};
+
+function dragonClicked(dragonType) {
+    if (eggCounts[dragonType] < MAX_EGG_COUNTS[dragonType]) {
+        // get the nest
+        const nest = document.getElementById('nest')
+        
+        // create an egg image
+        const eggImg = document.createElement('img')
+        let eggFile = 'img/' + dragonType + '_egg.png'
+        eggImg.src = eggFile
+        eggImg.className = 'egg-image ' + dragonType
+        // add the egg image to the nest
+        nest.appendChild(eggImg)
+        eggCounts[dragonType]++;
+
+    }
+    console.log('dragon clicked, type was ' + dragonType + ' egg counts are: ', eggCounts)
+}
